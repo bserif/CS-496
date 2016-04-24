b51ea4496d57fd59bc0f7763d3e2d2e37529190c
bserif <bserif2103@gmail.com>
2016-04-24 00:46:31 -0400

    Added Controller Code For API

---
+++ Assignment 3/CS496 Assignment 3/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using CS496_Assignment_3.Models;
+using CS496_Assignment_3.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CS496_Assignment_3.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private EmployeeRepository contactRepository;
+
+        public EmployeeController()
+        {
+            this.contactRepository = new EmployeeRepository();
+        }
+
+        public Employee[] Get()
+        {
+            return this.contactRepository.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Employee contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Employee>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+    }
+}
---
+++ Assignment 3/CS496 Assignment 3/Services/EmployeeRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CS496_Assignment_3.Models;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Configuration;
+using Microsoft.Azure;
+
+namespace CS496_Assignment_3.Services
+{
+    public class EmployeeRepository
+    {
+        private const string CacheKey = "ContactStore";
+
+        public EmployeeRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Employee[]
+                    {
+                        new Employee
+                        {
+                            Id = 1,
+                            Name = "Glenn Block"
+                        },
+                        new Employee
+                        {
+                            Id = 2,
+                            Name = "Dan Roth"
+                        }
+                    };
+
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+        public Employee[] GetAllContacts()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Employee[])ctx.Cache[CacheKey];
+            }
+
+            return new Employee[]
+            {
+                new Employee
+                {
+                    Id = 0,
+                    Name = "Placeholder"
+                }
+            };
+        }
+
+        public bool SaveContact(Employee contact)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Employee[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(contact);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
